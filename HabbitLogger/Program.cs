c7caacddfcd3136e76008f1c7910db38a4cb34b6
Sephydev <nicolas.bussiere@outlook.fr>
2026-04-13 13:27:17 +0200

    Implement main menu

--- HabbitLogger/Program.cs
+++ HabbitLogger/Program.cs
@@ -1,8 +1,57 @@
 ﻿using Microsoft.Data.Sqlite;
 
-createDB();
+CreateDB();
+RunApp();
 
-void createDB()
+void RunApp ()
+{
+    bool running = true;
+
+    while (running)
+    {
+        Console.Clear();
+        Console.WriteLine("Welcome to Habit Logger!");
+        Console.WriteLine("-------------------------------------------");
+        Console.WriteLine("Please select one of the following options:");
+        Console.WriteLine("\t- 0 to exit the application");
+        Console.WriteLine("\t- 1 to view your logged habits");
+        Console.WriteLine("\t- 2 to add a new habit log");
+        Console.WriteLine("\t- 3 to delete a logged habit");
+        Console.WriteLine("\t- 4 to update a logged habit");
+
+        string? userInput = Console.ReadLine();
+
+        switch (userInput)
+        {
+            case "0":
+                Console.WriteLine("Bye bye!");
+                running = false;
+                break;
+            case "1":
+                Console.WriteLine("UNDER CONSTRUCTION - Please be patient (Press Enter to continue)");
+                Console.ReadLine();
+                break;
+            case "2":
+                Console.WriteLine("UNDER CONSTRUCTION - Please be patient (Press Enter to continue)");
+                Console.ReadLine();
+                break;
+            case "3":
+                Console.WriteLine("UNDER CONSTRUCTION - Please be patient (Press Enter to continue)");
+                Console.ReadLine();
+                break;
+            case "4":
+                Console.WriteLine("UNDER CONSTRUCTION - Please be patient (Press Enter to continue)");
+                Console.ReadLine();
+                break;
+            default:
+                Console.WriteLine("You've entered an incorrect option. Please try again. (Press Enter to continue)");
+                Console.ReadLine();
+                break;
+        }
+    }
+}
+
+void CreateDB()
 {
     using var connection = new SqliteConnection(@"Data Source=habit-logger.db");
     {
@@ -15,7 +64,8 @@ void createDB()
             ID INTEGER PRIMARY KEY,
             HABIT TEXT,
             DATE TEXT,
-            QUANTITY INTEGER
+            QUANTITY INTEGER,
+            UNIT TEXT
         )
         ";
 
