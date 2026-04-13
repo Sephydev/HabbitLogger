d7cff2b28698519fb22931ffca7951362ad059af
Sephydev <nicolas.bussiere@outlook.fr>
2026-04-13 12:58:37 +0200

    Create empty db

--- HabbitLogger/Program.cs
+++ HabbitLogger/Program.cs
@@ -1,1 +1,26 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Microsoft.Data.Sqlite;
+
+createDB();
+
+void createDB()
+{
+    using var connection = new SqliteConnection(@"Data Source=habit-logger.db");
+    {
+        connection.Open();
+
+        using var command = connection.CreateCommand();
+
+        command.CommandText = @"
+            CREATE TABLE IF NOT EXISTS habits (
+            ID INTEGER PRIMARY KEY,
+            HABIT TEXT,
+            DATE TEXT,
+            QUANTITY INTEGER
+        )
+        ";
+
+        command.ExecuteNonQuery();
+
+        connection.Close();
+    }
+}
